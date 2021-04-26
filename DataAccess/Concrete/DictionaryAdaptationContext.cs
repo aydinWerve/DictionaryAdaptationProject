e9583efd6230905bbd756e06397be83a1bc4b64e
Merve Aydin <08aydinmeve@gmail.com>
2021-04-26 17:24:38 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/DictionaryAdaptationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using Entity.Concrete;
+
+namespace DataAccess.Concrete
+{
+    public class DictionaryAdaptationContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
